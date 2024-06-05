ee65ba27b8916c4905fbf38c2987749c0265bfc1
sabina <sabina90@tuta.io>
2024-06-05 11:06:26 +0200

    add input + basic password requirements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [password, setPassword] = useState('');
+
+  function handleChange(event) {
+     setPassword(event.target.value);
+     console.log(password)
+  }
+
+  const requirements = [
+    {id : 0, name: 'At least 6 characters long'},
+    {id : 1, name: 'At least 1 number'},
+    {id : 2, name: 'At least 1 uppercase letter'}
+  ];
+
+    if (password.length >= 6) {
+      requirements[0].color = 'green'
+      
+      
+    }
+     if (/\d/.test(password)) {
+      requirements[1].color = 'green';
+      
+    }
+  
+ 
   return (
     <div className="App">
-      <p>Hello world!</p>
+      <h1>Password</h1>
+      
+      <input onChange={handleChange}/>
+      <h3>{requirements.map(requirement => (<ul className="heading" style={ {color: requirement.color} } key={requirement.id}>{requirement.name}</ul>))}</h3>
+      
+      
     </div>
   );
 }
 
+
+
+ 
 export default App;
