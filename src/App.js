34ff06792dc5915d0631650b1effb0b28a3ed24c
sabina <sabina90@tuta.io>
2024-06-19 17:30:49 +0200

    update App component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
-import { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Heading from "./components/Heading";
+import Input from "./components/Input";
+import Condition from "./components/Condition";
 
 function App() {
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("");
 
-  function handleChange(event) {
-     setPassword(event.target.value);
-     console.log(password)
-  }
-
-  const requirements = [
-    {id : 0, name: 'At least 6 characters long'},
-    {id : 1, name: 'At least 1 number'},
-    {id : 2, name: 'At least 1 uppercase letter'}
-  ];
-
-    if (password.length >= 6) {
-      requirements[0].color = 'green'
-      
-      
-    }
-     if (/\d/.test(password)) {
-      requirements[1].color = 'green';
-      
-    }
-  
- 
   return (
     <div className="App">
-      <h1>Password</h1>
-      
-      <input onChange={handleChange}/>
-      <h3>{requirements.map(requirement => (<ul className="heading" style={ {color: requirement.color} } key={requirement.id}>{requirement.name}</ul>))}</h3>
-      
-      
+      <Heading />
+      <Input input={password} setInput={setPassword} />
+      <Condition password={password} />
     </div>
   );
 }
 
-
-
- 
 export default App;
